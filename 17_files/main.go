4dd7515b60e7aa01c6bda2504fe00329232f5181
Imnmania <niloyb@live.com>
2023-05-16 20:19:02 +0600

    files

---
+++ 17_files/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to files in golang")
+
+	content := "This needs to go inside a file - niloybiswas.me"
+	file, err := os.Create("./mygofile.txt")
+	checkNilErr(err)
+
+	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+
+	fmt.Println("Length is => ", length)
+	defer file.Close()
+	readFile("./mygofile.txt")
+}
+
+func readFile(filename string) {
+	dataByte, err := os.ReadFile(filename)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkNilErr(err)
+	fmt.Println("Text data inside the file is \n ", string(dataByte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
